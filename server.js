3bc33826c6a71f7a0f09d5c7aabc5c4184dc4fa1
stevekama <stevekamahertz@gmail.com>
2019-07-19 15:55:29 +0300

    Final update

--- server.js
+++ server.js
@@ -19,15 +19,29 @@ io.sockets.on('connection', function(socket){
     connections.push(socket);
     console.log('Connected: %s sockets connected', connections.length);
 
-    ///disconnect 
+    // disconnect 
     socket.on('disconnect', function(data){
+        users.slice(users.slice(users.indexOf(socket.username), 1));
+        updateUsernames();  
         connections.slice(connections.indexOf(socket), 1);
         console.log('Disconnected: %s sockets connected', connections.length);
     });
 
-    //send message 
+    // send message 
     socket.on('send message', function(data){
-        io.sockets.emit('new message', {msg: data});
+        io.sockets.emit('new message', {msg: data, user: socket.username});
     });
+
+    // New user
+    socket.on('new user', function(data, callback){
+        callback(true);
+        socket.username = data;
+        users.push(socket.username);
+        updateUsernames();
+    });
+
+    function updateUsernames(){
+        io.sockets.emit('get users', users);
+    }
    
 });
